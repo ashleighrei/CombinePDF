77b94c68e6ed856099b4f52afca95c9a0713295a
ashleighrei <146857292+ashleighrei@users.noreply.github.com>
2023-11-06 18:39:26 +0000

    Update python-combine-me.py

--- python-combine-me.py
+++ python-combine-me.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import os
 import PyPDF2
-from PyPDF2.generic import BooleanObject, NameObject
 from io import BytesIO
 import tempfile
+import subprocess
 
 # Custom CSS to set the font, colors, and font-weight
 css = f"""
@@ -25,13 +25,12 @@ st.markdown(css, unsafe_allow_html=True)
 
 st.markdown('<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">', unsafe_allow_html=True)
 
-# Function to combine and compress PDFs using PyPDF2
+# Function to combine and compress PDFs using PyPDF2 and ghostscript
 def combine_pdfs(input_files, output_file):
-    # Create a PDF merger object
-    pdf_merger = PyPDF2.PdfFileMerger()
-
     try:
-        # Iterate through input PDF files and append them to the merger
+        # Create a PDF merger object
+        pdf_merger = PyPDF2.PdfFileMerger()
+
         for input_file in input_files:
             pdf_merger.append(input_file)
 
@@ -40,23 +39,19 @@ def combine_pdfs(input_files, output_file):
         pdf_merger.write(pdf_buffer)
         pdf_buffer.seek(0)  # Reset the buffer position
 
-        # Apply the FlateDecode filter for lossless compression
-        pdf_writer = PyPDF2.PdfFileWriter()
-        pdf_reader = PyPDF2.PdfFileReader(pdf_buffer)
-        for page_num in range(pdf_reader.getNumPages()):
-            page = pdf_reader.getPage(page_num)
-            page.compressContentStreams()
-            pdf_writer.addPage(page)
-
-        # Save the compressed combined PDF with the specified output file name and PDF extension
+        # Save the combined PDF with the specified output file name and PDF extension
         with open(output_file, 'wb') as out_pdf:
-            pdf_writer.write(out_pdf)
+            out_pdf.write(pdf_buffer.read())
 
-        return True
+        # Compress the combined PDF using ghostscript
+        compressed_output_file = output_file.replace(".pdf", "_compressed.pdf")
+        subprocess.run(["gs", "-sDEVICE=pdfwrite", "-dCompatibilityLevel=1.4", "-dNOPAUSE", "-dQUIET", "-dBATCH", f"-sOutputFile={compressed_output_file}", output_file])
+
+        return compressed_output_file
 
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
-        return False
+        return None
 
 # Rest of your Streamlit app code (as provided previously)
 # ...
